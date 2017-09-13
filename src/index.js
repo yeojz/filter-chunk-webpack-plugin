008f4849bb1e5342b7587cda69ee3236c00fd0ee
Gerald Yeo <gerald@fusedthought.com>
2017-09-13 11:01:19 +0800

    Added preview option

--- src/index.js
+++ src/index.js
@@ -5,22 +5,38 @@ import pick from 'lodash.pick';
 class FilterChunkWebpackPlugin {
   constructor(options) {
     if (!Array.isArray(options.patterns)) {
-      throw new Error('The "patterns" option in FilterChunkWebpackPlugin should be an array');
+      throw new Error('[FilterChunkWebpackPlugin] The "patterns" option should be an array');
     }
 
-    this.filter = options.include === true ? pick : omit;
-    this.patterns = options.patterns;
+    this.options = Object.assign({
+      include: false,
+      patterns: [],
+      preview: false
+    }, options);
+  }
+  
+  previewOuput(matchingFiles) {
+    const action = this.options.include === true ? 'included' : 'excluded';
+
+    console.log('%s file(s) that will be %s:', matchingFiles.length, action);
+    matchingFiles.forEach((file) => console.log('     %s', file));
+    console.log('');
   }
 
   apply(compiler) {
-    const patterns = this.patterns;
+    const filter = this.options.include === true ? pick : omit;
 
+    
     compiler.plugin('emit', (compilation, callback) => {
       const files = Object.keys(compilation.assets);
-      const matchingFiles = multimatch(files, patterns);
+      const matchingFiles = multimatch(files, this.options.patterns);
 
-      // eslint-disable-next-line no-param-reassign
-      compilation.assets = this.filter(compilation.assets, matchingFiles);
+      if (this.options.preview) {
+        this.previewOutput(matchingFiles);
+      } else {
+        // eslint-disable-next-line no-param-reassign
+        compilation.assets = filter(compilation.assets, matchingFiles);
+      }
 
       callback();
     });
