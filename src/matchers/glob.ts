06b8741666732a6f8b57e167510e3170a2c9a28b
Gerald Yeo <me@yeojz.dev>
2026-01-27 11:32:37 +0800

    feat: implement glob matcher (green)

---
+++ src/matchers/glob.ts
@@ -0,0 +1,6 @@
+import picomatch from "picomatch";
+
+export function createGlobMatcher(pattern: string): (filename: string) => boolean {
+  const isMatch = picomatch(pattern);
+  return (filename: string) => isMatch(filename);
+}
